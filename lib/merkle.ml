984c3a6e4c75544e1a2d5a9305b31b18a6d951d7
Flydexo <flydexo@gmail.com>
2025-05-11 17:33:47 +0200

    merkle tree working

--- lib/merkle.ml
+++ lib/merkle.ml
@@ -1,4 +1,5 @@
 type tree = Leaf of string * string | Node of tree * tree * string
+type path = Left of string | Right of string
 
 type t = {
   len : int;
@@ -37,19 +38,25 @@ let get_authentication_path tree i =
   let rec aux path t i = match t.tree with
     | Leaf(_,_) when i = 0 -> path
     | Node(l,r,_) -> 
-      if i >= t.len/2 then
-        aux ((_root l)::path) {len=t.len/2;tree=r} (i-t.len/2)
-      else 
-        aux ((_root r)::path) {len=t.len - t.len/2;tree=l} i
+      if i >= t.len/2 then begin
+        Printf.printf "Left\n";
+        aux (Left((_root l))::path) {len=t.len/2;tree=r} (i-t.len/2)
+      end else begin 
+        Printf.printf "Right\n";
+        aux (Right((_root r))::path) {len=t.len - t.len/2;tree=l} i
+      end
     | _ -> failwith "Should not happen";
   in aux [] tree i
 
-let verify root path value = 
+let verify root (path:path list) value = 
   let rec aux computed_root = function
     | [] -> computed_root = root
     | h::t -> (
-      let k = hash (String.concat "" [h;computed_root]) in 
+      let k = Sha256.to_hex (Sha256.string (
+        match h with
+        | Left node -> String.concat "" [node; computed_root]
+        | Right node -> String.concat "" [computed_root; node]))
+      in
       Printf.printf "%s\n" k;
-      Printf.printf "%s\n" (String.concat "" [h;computed_root]);
       aux (k) t)
-  in aux "" (value::path)
+  in aux "" (Right(value)::path)
